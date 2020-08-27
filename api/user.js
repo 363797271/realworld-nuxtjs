5d64794f64e98d670aa3dcf4b886867fdf84628c
rt_zhangyingdan <363797271@qq.com>
2020-08-27 13:40:58 +0800

    first commit

---
+++ api/user.js
@@ -0,0 +1,20 @@
+// 用户模块
+import request from '@/utils/request'
+
+// 用户登录
+export const login = data => {
+  return request({
+    method: 'POST',
+    url: '/api/users/login',
+    data
+  })
+}
+
+// 用户注册
+export const register = data => {
+  return request({
+    method: 'POST',
+    url: '/api/users/register',
+    data
+  })
+}
---
+++ pages/login/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">{{ isLogin ? 'Sign in' : 'Sign up' }}</h1>
+          <p class="text-xs-center">
+            <nuxt-link v-if="isLogin" to="/register"
+              >Need an account?</nuxt-link
+            >
+            <nuxt-link v-else to="/login">Have an account?</nuxt-link>
+          </p>
+
+          <ul class="error-messages">
+            <li>That email is already taken</li>
+          </ul>
+
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group" v-if="!isLogin">
+              <input
+                v-model="user.username"
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Your Name"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                v-model="user.email"
+                class="form-control form-control-lg"
+                type="email"
+                placeholder="Email"
+                required
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                v-model="user.password"
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              {{ isLogin ? 'Sign in' : 'Sign up' }}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { login } from '@/api/user'
+export default {
+  name: 'loginRegister',
+  computed: {
+    isLogin() {
+      return this.$route.name === 'login'
+    }
+  },
+  data() {
+    return {
+      user: {
+        username: '',
+        email: '', // zyd001@qq.com
+        password: '' // 12345678
+      }
+    }
+  },
+  methods: {
+    async onSubmit() {
+      // 提交表单请求登录
+      const { data } = await login({
+        user: this.user
+      })
+
+      console.log(data)
+      // TODO：保存用户的登录状态
+
+      // 跳转到首页
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style></style>
